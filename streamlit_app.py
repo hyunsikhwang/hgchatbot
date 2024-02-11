9461bd76d39964c7a949e52dbef4a4500be34891
Hyunsik Hwang <gomting@naver.com>
2024-02-11 22:32:06 +0900

    st.write to st.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ if msg:
     msg_en = chatbot.chat(msg)
 
     st.markdown("#### 원문")
-    st.write(msg_en)
+    st.text(msg_en)
 
     msg_ko = translation(str(msg_en))
     st.markdown("#### 번역")
-    st.write(msg_ko)
+    st.text(msg_ko)
