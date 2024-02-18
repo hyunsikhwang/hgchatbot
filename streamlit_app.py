92e1d0d19ab5f17416b9f5944d5d6e91ce48c68d
Hyunsik <gomting@naver.com>
2024-02-18 17:06:55 +0900

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,9 @@ try:
 except:
     # Log in to huggingface and grant authorization to huggingchat
     sign = Login(email, passwd)
+    cookies = sign.login()
     sign.saveCookiesToDir(cookie_path_dir)
-    cookies = sign.loadCookiesFromDir(cookie_path_dir) # This will detect if the JSON file exists, return cookies if it does and raise an Exception if it's not.
+    # cookies = sign.loadCookiesFromDir(cookie_path_dir) # This will detect if the JSON file exists, return cookies if it does and raise an Exception if it's not.
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
 
 system_prompt = '''
